0375b8bf460e4d76e8d63bbe37d44f89ecfa3330
duongn6 <duongnguyen4823@gmail.com>
2025-06-04 11:40:52 +0700

    Update security.py

--- Website/Backend/app/utils/security.py
+++ Website/Backend/app/utils/security.py
@@ -91,8 +91,8 @@ async def get_current_user(
 
 async def get_current_active_user(current_user: User = Depends(get_current_user)):
     """Kiểm tra người dùng có active không"""
-    if not current_user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
+    # if not current_user.is_active:
+    #     raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
 
